185dbf00e8b29482c584021c414b6bdbeb4d8866
TrofimGest <gubaoslika@gmail.com>
2022-05-02 20:03:55 +0300

    feat: add basic project structure

---
+++ src/components/NoRepos.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NoRepos() {
+  return <div>NoRepos</div>;
+}
+
+export default NoRepos;
---
+++ src/components/NoUser.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NoUser() {
+  return <div>NoUser</div>;
+}
+
+export default NoUser;
