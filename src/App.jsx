0895e5efb1da9467b9072114437bf30c065000f8
TrofimGest <gubaoslika@gmail.com>
2022-05-03 00:41:46 +0300

    feat: add start component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import Start from './components/Start';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Start />
     </div>
   );
 }
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styles from './Start.module.css';
 
 function Start() {
-  return <div>Start</div>;
+  return (
+    <section className={styles.start}>
+      <div className={styles.logo} />
+      <p className={styles.text}>Start with searching a GitHub user</p>
+    </section>
+  );
 }
 
 export default Start;
