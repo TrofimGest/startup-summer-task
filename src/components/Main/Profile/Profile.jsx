44cf505e3a6f5960034abdd6e6b062d93404f020
TrofimGest <gubaoslika@gmail.com>
2022-05-05 20:47:48 +0300

    feat: add a mechanism for displaying information in the profile component

--- src/components/Main/Profile/Profile.jsx
+++ src/components/Main/Profile/Profile.jsx
@@ -4,15 +4,16 @@ import followingIcon from '../../../assets/icons/following.svg';
 import styles from './Profile.module.css';
 
 function Profile({ data }) {
+  console.log(data);
   return (
     <div className={styles.container}>
       <div className={styles.avatar}>
-        <img src="https://avatars.githubusercontent.com/u/73033225?v=4" alt="avatar" />
+        <img src={data.avatar_url} alt="avatar" />
       </div>
-      <div className={styles.name}>Gest Trafim</div>
+      <div className={styles.name}>{data.name}</div>
       <div className={styles.login}>
         <a href="https://github.com/TrofimGest" rel="noreferrer" target="_blank">
-          trofimgest
+          {data.login}
         </a>
       </div>
       <div className={styles.follows}>
@@ -20,13 +21,13 @@ function Profile({ data }) {
           <div className={styles['icon-container']}>
             <img className={styles['followers-img']} src={followersIcon} alt="followers icon" />
           </div>
-          <p> followers</p>
+          <p>{data.followers} followers</p>
         </div>
         <div className={styles.following}>
           <div className={styles['icon-container']}>
             <img className={styles['following-img']} src={followingIcon} alt="following icon" />
           </div>
-          <p> following</p>
+          <p>{data.following} following</p>
         </div>
       </div>
     </div>
