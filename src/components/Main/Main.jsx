8f49a117d3261fadf8149cdd133e603724ca16b8
TrofimGest <gubaoslika@gmail.com>
2022-05-07 19:42:57 +0300

    feat: improve request logics and add a conditional rendering for main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Profile from './Profile/Profile';
 import Repos from './Repos/Repos';
+import NotFound from '../NotFound/NotFound';
 import styles from './Main.module.css';
 
 function Main({ profileData, reposData }) {
   return (
     <div className={styles.container}>
-      <Profile profileData={profileData} />
-      <Repos profileData={profileData} reposData={reposData} />
+      {profileData.status !== 200 ? (
+        <NotFound />
+      ) : (
+        <>
+          <Profile profileData={profileData.data} />
+          <Repos profileData={profileData.data} reposData={reposData} />
+        </>
+      )}
     </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -4,19 +4,27 @@ import env from 'react-dotenv';
 const token = env.GITHUB_TOKEN;
 
 export async function getUser(username) {
-  const response = await axios.get(`https://api.github.com/users/${username}`, {
-    headers: {
-      Authorization: { token }
-    }
-  });
-  return response.data;
+  const response = await axios
+    .get(`https://api.github.com/users/${username}`, {
+      headers: {
+        Authorization: { token }
+      }
+    })
+    .catch((error) => {
+      return error.response;
+    });
+  return response;
 }
 
 export async function getRepos(username) {
-  const response = await axios.get(`https://api.github.com/users/${username}/repos`, {
-    headers: {
-      Authorization: { token }
-    }
-  });
+  const response = await axios
+    .get(`https://api.github.com/users/${username}/repos`, {
+      headers: {
+        Authorization: { token }
+      }
+    })
+    .catch((error) => {
+      return error.response;
+    });
   return response.data;
 }
